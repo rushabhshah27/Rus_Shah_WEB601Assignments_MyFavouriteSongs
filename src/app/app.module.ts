761fb5f91b81080b1377deec7d609a5d2a51d666
Rushabh Rajeshkumar Shah <W0800532@myscc.ca>
2023-07-12 14:49:31 -0400

    Assignment-6 with Message Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentTypeFilterPipe } from './content-type-filter.pipe';
 import { HoverAffectDirective } from './hover-affect.directive';
-import { CreateContentComponent } from './create-content/create-content.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +17,7 @@ import { CreateContentComponent } from './create-content/create-content.componen
     ContentListComponent,
     ContentTypeFilterPipe,
     HoverAffectDirective,
-    CreateContentComponent
+    MessageComponent
   ],
   imports: [
     BrowserModule,
